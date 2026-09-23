a996de6cf37e370ab565bdb6ce492f8e5709b49c
zhyeonn <jihyeon692@gmail.com>
2026-09-23 22:29:20 +0900

    feat: 홈 화면 프론트엔드 구현 및 API 연동 #71

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
+import { HomePage } from './pages/home/ui/HomePage';
+
 /** MULO 프론트엔드의 최상위 애플리케이션 컴포넌트다. */
 function App() {
-  return (
-    <main className="app-shell">
-      <h1>MULO</h1>
-      <p>프론트엔드 초기 설정이 완료되었습니다.</p>
-    </main>
-  );
+  return <HomePage />;
 }
 
 export default App;
